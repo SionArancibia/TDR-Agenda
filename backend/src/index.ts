eb3746cf20449ecb3078f49940873aa755c2ba06
Sion Arancibia <sionarancibia@gmail.com>
2024-09-07 22:30:57 -0400

    feat: authentication by role

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import authRoutes from "./routes/auth"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.listen(3000,()=> {
-    console.log("hola server");
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT,()=> {
+    console.log("Server running on port: ", PORT);
 });
---
+++ backend/src/middleware/authorizeRole.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+
+const authorizeRole = (allowedRoles: string[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (!req.user) {
+            return res.status(401).json({ error: "Unauthorized - No user data available" });
+        }
+
+        const { role } = req.user;
+
+        if (!allowedRoles.includes(role)) {
+            return res.status(403).json({ error: "Forbidden - You do not have permission to access this resource" });
+        }
+
+        next();
+    };
+};
+
+export default authorizeRole;
